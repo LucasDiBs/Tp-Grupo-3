c0c72473571db613423fae19095ddda70dea1b30
HungusBogassi <albertopaladea@gmail.com>
2024-06-27 19:21:55 -0300

    trabajando

--- src/main/java/com/unla/grupo03/controller/StockController.java
+++ src/main/java/com/unla/grupo03/controller/StockController.java
@@ -9,23 +9,23 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.unla.grupo03.model.Stock;
-import com.unla.grupo03.service.StockService;
+//import com.unla.grupo03.model.Stock;
+//import com.unla.grupo03.service.StockService;
 
 @Controller
 @RequestMapping("admin/stock")
 public class StockController {
 	
-	@Autowired
-	private StockService service;
-	
-	@ModelAttribute
-	@GetMapping({"", "/"})
-	private String listarProduct(Model model) {
-
-		List<Stock> stock = service.listar();
-		model.addAttribute("stock", stock);
-		
-		return "admin/stock";
-	}
+//	@Autowired
+//	private StockService service;
+//	
+//	@ModelAttribute
+//	@GetMapping({"", "/"})
+//	private String listarProduct(Model model) {
+//
+//		List<Stock> stock = service.listar();
+//		model.addAttribute("stock", stock);
+//		
+//		return "admin/stock";
+//	}
 }
